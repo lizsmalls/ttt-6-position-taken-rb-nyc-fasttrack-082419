0b66ed4bb145596fafee468b8dd59499d188690e
Elizabeth Smalls <lizsmalls@hotmail.com>
2019-08-24 19:59:59 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,6 +4,17 @@ def position_taken(board, index)
   if board[index] == "  " || board[index] == " "
     return false
   end
- end 
-    
-    if board[index ]
+ end
+ 
+def position_taken(board, index)
+  if board[index] == nil || board[index] == " "
+      return false
+    elsif board[index] == " "
+    return false
+    elsif board [index] == "X"  || board[index]  == "0"
+      return true
+    end
+  end
+  end
+ end
+ 
